ba6c8acd4283c10cddfab64bb37a28400d97e1a4
John Faber Florez Vasco <faber-vasco@Fabers-MacBook-Air.local>
2025-09-18 12:43:02 -0500

    Se creó la función para verificar si se puede sentar en el trono

--- trono_hierro.py
+++ trono_hierro.py
@@ -1,3 +1,14 @@
+# Funcion para definir quien se puede sentr en el trono de hierro
+def puede_sentarse_en_el_trono(nombre, casa):
+    mensaje = f"cualquier cosa {nombre} {casa}"
+    return mensaje
+
 # Datos de Entrada
 nombre_personaje = input("Ingrese el nombre del personaje: ")
 casa_eot = input("Ingrese el nombre de la casa a la que pertenece: ")
+
+#Proceso
+mensaje = puede_sentarse_en_el_trono(nombre_personaje, casa_eot)
+
+#salida
+print(mensaje)
