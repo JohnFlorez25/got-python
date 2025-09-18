321ce0be13d0a8069fe52f3bc0c61665497e2183
John Faber Flórez Vasco <john.florez25@gmail.com>
2025-09-18 12:24:09 -0500

    Rename variable 'nombre' to 'nombre_personaje'

--- trono_hierro.py
+++ trono_hierro.py
@@ -1,3 +1,3 @@
 # Datos de Entrada
-nombre = input("Ingrese el nombre del personaje: ")
+nombre_personaje = input("Ingrese el nombre del personaje: ")
 casa = input("ingrese el nombre de la casa a la que pertenece: ")
