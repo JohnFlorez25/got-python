f8341acf813c06c0a49117ab6e65e55f59e0b4ff
MiguelSalazar24 <miguelsalazarag247@gmail.com>
2025-09-18 12:28:53 -0500

    Update trono_hierro.py

--- trono_hierro.py
+++ trono_hierro.py
@@ -1,3 +1,3 @@
 # Datos de Entrada
 nombre_personaje = input("Ingrese el nombre del personaje: ")
-casa = input("ingrese el nombre de la casa a la que pertenece: ")
+casa = input("Ingrese el nombre de la casa_eot a la que pertenece: ")
